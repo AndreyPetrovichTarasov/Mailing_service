4814cacd8335bb7dd54aea6014a7020ffb73414a
lacry <lacryk@gmail.com>
2024-10-27 11:49:45 +0300

    moduls

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=255)
+    comment = models.TextField()
+
+    def __str__(self):
+        return self.email
--- mailings/models.py
+++ mailings/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils import timezone
+from clients.models import Client
+from messages_list.models import Message
 
-# Create your models here.
+
+class Mailing(models.Model):
+    STATUS_CHOICES = [
+        ('Создана', 'Создана'),
+        ('Запущена', 'Запущена'),
+        ('Завершена', 'Завершена'),
+    ]
+
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Создана')
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    recipients = models.ManyToManyField(Client)
+
+    def __str__(self):
+        return f'Рассылка {self.id} ({self.status})'
+
+
+class MailingAttempt(models.Model):
+    STATUS_CHOICES = [
+        ('Успешно', 'Успешно'),
+        ('Не успешно', 'Не успешно'),
+    ]
+
+    mailing = models.ForeignKey(Mailing, on_delete=models.CASCADE)
+    attempt_time = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    response = models.TextField()
+
+    def __str__(self):
+        return f'Попытка {self.id} для рассылки {self.mailing.id}'
